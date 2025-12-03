0291e43b652d17cc780143649dbb3af2d73bdb09
dashaBobrovad <dbobrova.dasha@gmail.com>
2025-12-03 17:08:15 +0300

    gitignore & clean

--- dist/scripts/search.js
+++
@@ -1,8 +0,0 @@
-export function initSearch(searchEl, state) {
-  const input = searchEl.querySelector('.search__input');
-
-  input.addEventListener('input', (event) => {
-    state.searchQuery = event.target.value.toLowerCase();
-    state.onChange && state.onChange();
-  });
-}
